dcab07e482892b70b9b2f5694cd9b8605f75b4ae
Apurv Chandra <63102689+apurvchandra@users.noreply.github.com>
2023-05-10 08:29:51 +0100

    updated for azure authorities (#1)
    * updated for azure authorities
    
    * Update README.md (#2)

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,42 +1,72 @@
 package hello;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController extends WebSecurityConfigurerAdapter{
+@EnableMethodSecurity 
+@EnableWebSecurity
+public class HelloController  {
+    Logger logger = LogManager.getLogger(HelloController.class);
 
-@RequestMapping("/user")
-public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-return Collections.singletonMap("name",principal.getAttribute("name"));
-}
+    @RequestMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
 
-@Override
-protected void configure(HttpSecurity http) throws Exception {
-    http
-    .authorizeRequests(a -> a
-        .antMatchers("/", "/error", "/webjars/**").permitAll()
-        .anyRequest().authenticated()
-    )
-    .exceptionHandling(e -> e
-        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-    )
-    .csrf(c -> c
-        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-    )
-    .logout(l -> l
-        .logoutSuccessUrl("/").permitAll() 
-    )
-    .oauth2Login();
-}
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('WEB_ADMIN')")
+    public ResponseEntity<String> admin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        logger.error("APURV: "+SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString());
+        String username = authentication.getName();
+        return ResponseEntity.ok("Hello, " + username + " (Admin)");
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<String> users() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return ResponseEntity.ok("Hello, " + username + " (Users)");
+    }
+
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+
 }
